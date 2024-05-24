92d1cc277c6c62e40defc55262fab7df92d5856b
Dmitriy Sidorov <dimasidorov734@gmail.com>
2024-05-25 00:14:57 +0300

    initial commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import jm.task.core.jdbc.model.User;
 
-/**
- * Created by vserdiuk on 2/4/17.
- */
+
 public class Util {
 
     private static final Logger logger = Logger.getLogger(Util.class.getName());
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/user";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root1";
@@ -20,14 +20,29 @@ public class Util {
     public static Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName(DRIVER);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            logger.log(Level.INFO, "Connection is OK");
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             logger.log(Level.SEVERE, "Connection has ERROR", e);
         }
         return connection;
+    }
+    public static class Hibernate{
+        private static final Configuration configuration = new Configuration()
+                .setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.url", URL)
+                .setProperty("hibernate.connection.username", USERNAME)
+                .setProperty("hibernate.connection.password", PASSWORD)
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.current_session_context_class", "thread")
+                .addAnnotatedClass(User.class);
+        private static final SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        public static SessionFactory getSessionFactory() {
+            return sessionFactory;
+        }
+
     }
 
 }
